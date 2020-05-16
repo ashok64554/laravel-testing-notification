f2a386c626a83325dc0461c0438da70b5632f0ee
Peter Fox <peter.fox@peterfox.me>
2020-05-16 14:08:51 +0100

    Sets up desktop notifications on tests passing/failing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use NunoMaduro\LaravelDesktopNotifier\Notification;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Event::listen(CommandFinished::class, function (CommandFinished $event) {
+            if ($event->command === 'test') {
+                $passed = !$event->exitCode;
+                $this->notify($passed);
+            }
+        });
+    }
+
+    public function notify($passed)
+    {
+        $notification = (new Notification())
+            ->setTitle(config('app.name') . ' Tests')
+            ->setBody(
+                $passed ?
+                    'All tests passed!' :
+                    'One or more tests failed!'
+            )
+            ->addOption( // ignore this part if you don't use a Mac
+                'sound',
+                $passed ?
+                    'glass' :
+                    'basso'
+            );
+
+        $this->app->make('desktop.notifier')->send($notification);
     }
 }
